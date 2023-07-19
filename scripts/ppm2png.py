51cb25641168aaeba586785ac4d2d1734ac92e9d
Frederic <f.degroef@gmail.com>
2023-07-19 11:55:58 +0200

    ppm2png supports directory of files to convert

--- scripts/ppm2png.py
+++ scripts/ppm2png.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
 import imageio as io
 import sys
+import os
 
-if __name__ == '__main__':
-    src_fpath = sys.argv[1]
+def ppm2png(src_fpath):
     im = io.v3.imread(src_fpath)
     dst_fpath = src_fpath.replace(".ppm", ".png")
     io.v3.imwrite(dst_fpath, im)
+
+if __name__ == '__main__':
+    src_fpath = sys.argv[1]
+    if os.path.isfile(src_fpath):
+        ppm2png(src_fpath)
+    elif os.path.isdir(src_fpath):
+        for each in os.listdir(src_fpath):
+            if each.endswith(".ppm"):
+                ppm2png(os.path.join(src_fpath, each))
