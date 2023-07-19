d4b74ca735302ec4048d072d464669437e76e257
Frederic <f.degroef@gmail.com>
2023-07-19 12:15:54 +0200

    move ppmwrite to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,11 @@
+mod ppmio;
 mod ray;
 mod types;
 
+use crate::ppmio::ppmwrite;
 use crate::ray::Ray;
 use crate::types::{dot, lerp, Color, ImageRGBA, Point, Vec3, RED, WHITE};
-use std::fs::File;
-use std::io::{BufWriter, Write};
-
-fn ppmwrite(fname: &str, im: ImageRGBA) {
-    let f = File::create(fname).expect("Unable to create file");
-    let mut f = BufWriter::new(f);
-    let w = im.width;
-    let h = im.height;
-    let header = format!("P3\n{w} {h}\n255\n");
-
-    f.write_all(header.as_bytes())
-        .expect("unable to write data");
-    let count = w * h;
-    for i in 0..count {
-        let r = im.pixels[i * 4];
-        let g = im.pixels[i * 4 + 1];
-        let b = im.pixels[i * 4 + 2];
-
-        f.write_fmt(format_args!("{r} {g} {b}\n"))
-            .expect("unable to write data");
-    }
-}
+use std::io::Write;
 
 fn hit_sphere(center: &Point, radius: f32, r: &Ray) -> f32 {
     let oc = &r.orig - &center;
---
+++ src/ppmio.rs
@@ -0,0 +1,23 @@
+use crate::types::ImageRGBA;
+use std::fs::File;
+use std::io::{BufWriter, Write};
+
+pub fn ppmwrite(fname: &str, im: ImageRGBA) {
+    let f = File::create(fname).expect("Unable to create file");
+    let mut f = BufWriter::new(f);
+    let w = im.width;
+    let h = im.height;
+    let header = format!("P3\n{w} {h}\n255\n");
+
+    f.write_all(header.as_bytes())
+        .expect("unable to write data");
+    let count = w * h;
+    for i in 0..count {
+        let r = im.pixels[i * 4];
+        let g = im.pixels[i * 4 + 1];
+        let b = im.pixels[i * 4 + 2];
+
+        f.write_fmt(format_args!("{r} {g} {b}\n"))
+            .expect("unable to write data");
+    }
+}
