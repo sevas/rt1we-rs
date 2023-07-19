7ab7fa212c5a50e3b092965cf433b1d3318e924c
Frederic <f.degroef@gmail.com>
2023-07-19 21:37:34 +0200

    add some comments on the sphere hitting method

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,21 @@ use crate::ray::Ray;
 use crate::types::{dot, lerp, Color, ImageRGBA, Point, Vec3, RED, WHITE};
 use std::io::Write;
 
+///
 fn hit_sphere(center: &Point, radius: f32, r: &Ray) -> f32 {
+    // Sphere hits are the points where:
+    //      x^2 + y^2 + z^2 - r^2 = 0
+    // For a sphere of radius r, center C, it's all the points P satisfying:
+    //      (P-C)·(P-C) - r^2 = 0
+    // Equation is rewritten in terms of vectors, parametrized by variable t:
+    //      (A + t*b - C)·(A + t*b - C) - r^2 = 0
+    // Sphere hit is now finding the roots of the univariate quadratic equation (parametrized by t):
+    //      t^2 * b·b + t * 2*b·(A-C) + (A-C)·(A-C) - r^2 = 0
+    // with:
+    //      b = r.dir
+    //      A = r.orig
+    //      C = center
+    //      r = radius
     let oc = &r.orig - &center;
     let a = dot(&r.dir, &r.dir);
     let b = 2.0 * dot(&oc, &r.dir);
@@ -78,13 +92,13 @@ fn render() -> ImageRGBA {
         z: 0.0,
     };
     let horizontal = Vec3 {
-        x: vp_width as f32,
+        x: vp_width,
         y: 0.0,
         z: 0.0,
     };
     let vertical = Vec3 {
         x: 0.0,
-        y: vp_height as f32,
+        y: vp_height,
         z: 0.0,
     };
     let lower_left_corner = &origin
@@ -103,8 +117,8 @@ fn render() -> ImageRGBA {
             let u = i as f32 / (im.width as f32 - 1.0);
             let v = j as f32 / (im.height as f32 - 1.0);
             let ray = Ray {
-                orig: origin.into(),
-                dir: &lower_left_corner + &(&horizontal * u) + (&vertical * v) - origin.into(),
+                orig: origin,
+                dir: &lower_left_corner + &(horizontal * u) + (vertical * v) - origin,
             };
             let pixel_color = ray_color(&ray);
             let ir = (pixel_color.x * 255.0) as u8;
