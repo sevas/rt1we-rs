96fd62961568c77e85287ff4d17cb34d77481374
Frederic <f.degroef@gmail.com>
2023-07-19 12:10:37 +0200

    sphere normals rendering

--- src/main.rs
+++ src/main.rs
@@ -26,24 +26,31 @@ fn ppmwrite(fname: &str, im: ImageRGBA) {
 }
 
 
-fn hit_sphere(center: &Point, radius: f32, r: &Ray) -> bool{
+fn hit_sphere(center: &Point, radius: f32, r: &Ray) -> f32 {
     let oc = &r.orig - &center;
     let a = dot(&r.dir, &r.dir);
     let b = 2.0 * dot(&oc, &r.dir);
-    let c = dot(&oc, &oc) - radius*radius;
-    let disc = b*b - 4.0*a*c;
-    disc > 0.0
+    let c = dot(&oc, &oc) - radius * radius;
+    let disc = b * b - 4.0 * a * c;
+
+    if disc < 0.0 {
+        return -1.0;
+    }
+    else {
+        (-b - disc.sqrt()) / (2.0*a)
+    }
 }
 
 fn ray_color(r: &Ray) -> Color {
+    let t =  hit_sphere(&Point { x: 0.0, y: 0.0, z: -1.0 }, 0.5, r);
 
-    if (hit_sphere(&Point{x:0.0, y:0.0, z:-1.0}, 0.5, r)){
-        return RED;
+    if t > 0.0 {
+        let n = (r.at(t) - Vec3{x: 0.0, y: 0.0, z: -1.0}).normed();
+        return 0.5 * Color {x: n.x + 1.0, y: n.y + 1.0, z: n.z + 1.0}
     }
-
     let unit_direction = &r.dir.normed();
     let t = 0.5 * (unit_direction.y + 1.0);
-    lerp(&WHITE, &Color { x: 0.5, y: 0.7, z: 1.0 }, 1.0-t)
+    lerp(&WHITE, &Color { x: 0.5, y: 0.7, z: 1.0 }, 1.0 - t)
 }
 
 
@@ -60,9 +67,9 @@ fn render() -> ImageRGBA {
     let focal_length = 1.0;
 
     let origin = Point { x: 0.0, y: 0.0, z: 0.0 };
-    let horizontal = Vec3{x: vp_width as f32, y: 0.0, z: 0.0};
-    let vertical = Vec3{x: 0.0, y: vp_height as f32, z: 0.0};
-    let lower_left_corner = &origin - &(horizontal / 2.0) - (vertical / 2.0) - Vec3{x: 0.0, y: 0.0, z: focal_length};
+    let horizontal = Vec3 { x: vp_width as f32, y: 0.0, z: 0.0 };
+    let vertical = Vec3 { x: 0.0, y: vp_height as f32, z: 0.0 };
+    let lower_left_corner = &origin - &(horizontal / 2.0) - (vertical / 2.0) - Vec3 { x: 0.0, y: 0.0, z: focal_length };
 
 
     for j in (0..im.height).rev() {
@@ -90,6 +97,6 @@ fn render() -> ImageRGBA {
 
 fn main() {
     let im = render();
-    ppmwrite("out/image002.ppm", im);
+    ppmwrite("out/image003.ppm", im);
 }
 
