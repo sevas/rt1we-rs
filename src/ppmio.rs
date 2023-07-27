0764faddb5a9879588d82774d6d6cce77562ee5b
Frederic <f.degroef@gmail.com>
2023-07-27 16:32:37 +0200

    always flip image array vertically before export to ppm

--- src/ppmio.rs
+++ src/ppmio.rs
@@ -1,4 +1,4 @@
-use crate::image::ImageRGBA;
+use crate::image::{flipv, ImageRGBA};
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
@@ -9,6 +9,7 @@ pub fn ppmwrite(fname: &str, im: ImageRGBA) {
     let h = im.height;
     let header = format!("P3\n{w} {h}\n255\n");
 
+    let im = flipv(&im);
     f.write_all(header.as_bytes())
         .expect("unable to write data");
     let count = w * h;
