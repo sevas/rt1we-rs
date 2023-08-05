1de87dc403de2bef2a6f5a0d3ad35cae6546d221
Frederic <f.degroef@gmail.com>
2023-08-05 11:18:40 +0200

    make an mp4 and a mov

--- scripts/make_movie.sh
+++ scripts/make_movie.sh
@@ -2,3 +2,4 @@
 
 cd ../out || exit
 ffmpeg  -framerate 24 -i anim_image_%05d.ppm -vcodec libx264 -pix_fmt yuv420p output.mov
+ffmpeg  -framerate 24 -i anim_image_%05d.ppm -vcodec libx264 output.mp4
