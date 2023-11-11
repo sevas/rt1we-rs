bbcf541f36956accdff91d36a3e4fbfa6fbb351e
Frederic <f.degroef@gmail.com>
2023-11-11 12:54:10 +0100

    setup egui app

--- rt1we_gui/src/main.rs
+++ rt1we_gui/src/main.rs
@@ -1,2 +1,51 @@
 extern crate rt1we_renderer;
-fn main() {}
+
+use eframe::egui;
+
+fn main() -> Result<(), eframe::Error> {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(800.0, 600.0)),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "My egui App",
+        options,
+        Box::new(|cc| {
+            // This gives us image support:
+            egui_extras::install_image_loaders(&cc.egui_ctx);
+
+            Box::<MyApp>::default()
+        }),
+    )
+}
+
+struct MyApp {
+    width: u32,
+    height: u32,
+    max_depth: u32,
+    samples_per_pixel: u32,
+}
+
+impl Default for MyApp {
+    fn default() -> Self {
+        Self { width: 160, height: 120, max_depth: 50, samples_per_pixel: 100 }
+    }
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("rt1we-gui");
+            ui.horizontal(|ui| {
+                let name_label = ui.label("Width: ");
+                ui.text_edit_singleline(&mut self.name).labelled_by(name_label.id);
+            });
+            ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
+            if ui.button("Click each year").clicked() {
+                self.age += 1;
+            }
+            ui.label(format!("Hello '{}', age {}", self.name, self.age));
+        });
+    }
+}
